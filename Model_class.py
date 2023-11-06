6f046d420297030fea75107709a9dc20df1368f3
JohnF1103 <87583884+JohnF1103@users.noreply.github.com>
2023-11-05 23:35:41 -0500

    model fix import torch

--- Model_class.py
+++ Model_class.py
@@ -1,20 +1,16 @@
+import torch
+
 class Model(torch.nn.Module):
     def __init__(self, backbone):
         super().__init__()
 
-        # 선학습 모델에서는 특징 추출 부분만 사용하고 
-        # (N,3,380,280)->(N,1792,12,12)
         self.features = backbone
         
         # (N,1792,12,12)->(N,1792,1,1)
         self.avg_pool = torch.nn.AdaptiveAvgPool2d(output_size=(1, 1))
 
         self.classifier = torch.nn.Sequential(
-            ##############################################
-            # WRITE YOUR CODE HERE
-            # 여기서 self.features가 출력하는 모양을 flatten 했을 때
-            # 노드 수를 계산하여 
-            # 계산된 노드수->100으로 가는 완전 연결층을 구성하기
+        
             torch.nn.Linear(backbone._fc.in_features, 1024),
             torch.nn.BatchNorm1d(1024),
             torch.nn.ReLU(),
@@ -25,7 +21,6 @@ class Model(torch.nn.Module):
             torch.nn.Dropout(),
             torch.nn.Linear(512, 100),
             torch.nn.LogSoftmax(dim=-1)
-            ##############################################
         )
     
     def forward(self, x):
